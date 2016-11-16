48262a13b2636f92ae96d6fcf12f0e1f88b2e6e2
Melissa <melissa.palermo@hotmail.com>
2016-11-16 21:29:40 -0200

    dyghaph

---
+++ series tf.R
@@ -0,0 +1,33 @@
+#leitura banco de dados
+
+#para ex 1: (import dataset)
+
+vendas <- ex1
+
+#para ex 2: (import dataset)
+soi=ex22
+
+#formatar data:
+install.packages("lubridate")
+library(lubridate)
+
+vendas$V1 = dmy(vendas$V1) #acho que nao precisa disso
+
+
+#formatar serie temporal:
+
+serie_1=ts(vendas$V2, frequency = 12, start = c(2000, 1, calendar = TRUE)) 
+serie_2=ts(soi, frequency = 12, start =c(1866, calendar = TRUE) )
+
+
+#analises descritivas:
+install.packages("dygraphs")
+library(dygraphs)
+dygraph(serie_1, main = "Volume de vendas no varejo de tecidos, vestuários e calçados") %>% dyRangeSelector(dateWindow = c("2000-01-01", "2011-12-01"))
+
+dygraph(serie_2, main="Diferença de pressão normalizada entre Tahiti e Darwin") %>% dyRangeSelector
+
+#salvar como html que continua dinâmico.
+
+
+
